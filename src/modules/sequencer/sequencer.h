7b5e47f1eacc1445d967f38ef211a8d5a3623c1e
gehelem <gilles@joag.fr>
2023-01-09 14:58:09 +0100

    Add sequencer files
    (inspector module duplication)

---
+++ src/modules/sequencer/sequencer.h
@@ -0,0 +1,83 @@
+#ifndef SEQUENCER_MODULE_h_
+#define SEQUENCER_MODULE_h_
+#include <indimodule.h>
+
+#if defined(SEQUENCER_MODULE)
+#  define MODULE_INIT Q_DECL_EXPORT
+#else
+#  define MODULE_INIT Q_DECL_IMPORT
+#endif
+
+class MODULE_INIT SequencerModule : public IndiModule
+{
+    Q_OBJECT
+
+    public:
+        SequencerModule(QString name,QString label,QString profile,QVariantMap availableModuleLibs);
+        ~SequencerModule();
+
+    signals:
+
+        void RequestFrameResetDone();
+        void FrameResetDone();
+        void RequestExposureDone();
+        void ExposureDone();
+        void FindStarsDone();
+        void NextLoop();
+        void LoopFinished();
+        void ComputeDone();
+        void InitDone();
+
+        void RequestExposureBestDone();
+        void ExposureBestDone();
+        void ComputeResultDone();
+        void InitLoopFrameDone();
+        void LoopFrameDone();
+        void NextFrame();
+
+        void blobloaded();
+        void cameraAlert();
+        void AbortDone();
+        void Abort();
+    public slots:
+        void OnMyExternalEvent(const QString &eventType, const QString  &eventModule, const QString  &eventKey, const QVariantMap &eventData) override;
+        void OnSucessSEP();
+
+    private:
+        void newNumber(INumberVectorProperty *nvp) override;
+        void newBLOB(IBLOB *bp) override;
+        void newSwitch(ISwitchVectorProperty *svp) override;
+
+
+
+        void Shoot();
+        void SMAlert();
+        //void SMLoadblob(IBLOB *bp);
+        void SMLoadblob();
+        void SMAbort();
+        void startCoarse();
+
+        QString _camera  = "CCD Simulator";
+        bool    _newblob;
+
+        QPointer<fileio> _image;
+        Solver _solver;
+        FITSImage::Statistic stats;
+
+        int    _iterations = 3;
+        int    _steps = 3000;
+        int    _loopIterations = 2;
+        int    _loopIteration;
+        double _loopHFRavg;
+        double _exposure = 2;
+
+        int    _iteration;
+        double _bestpos;
+        double _bestposfit;
+        double _besthfr;
+
+};
+
+extern "C" MODULE_INIT SequencerModule *initialize(QString name,QString label,QString profile,QVariantMap availableModuleLibs);
+
+#endif
