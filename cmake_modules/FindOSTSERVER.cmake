75a55478df46be2b2bb5027c9bcfb757006681a6
gehelem <gilles@joag.fr>
2023-01-13 23:45:24 +0100

    keep this simple, Gilles please

--- cmake_modules/FindOSTSERVER.cmake
+++ cmake_modules/FindOSTSERVER.cmake
@@ -1,21 +1,14 @@
-include(FindPackageHandleStandardArgs)
+FIND_PATH(OST_INCLUDE_DIR indimodule.h)
+SET(OST_NAMES ${OST_NAMES} libostindimodule.so libostbasemodule.so)
+FIND_LIBRARY(OST_LIBRARY_BASE NAMES ${OST_NAMES} )
 
-SET(OST_NAMES ${OST_NAMES}  ostindimodule ostbasemodule)
-find_library(OST_LIBRARY_BASE NAMES ostbasemodule)
-find_library(OST_LIBRARY_INDI NAMES ostindimodule)
-
+# handle the QUIETLY and REQUIRED arguments and set OST_FOUND to TRUE if 
+# all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-#find_package_handle_standard_args(OST REQUIRED_VARS OST_LIBRARY OST_INCLUDE_DIR)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OST DEFAULT_MSG  OST_LIBRARY_INDI OST_LIBRARY_BASE OST_INCLUDE_DIR)
-find_path(OST_INCLUDE_DIR indimodule.h basemodule.h)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OST DEFAULT_MSG OST_INCLUDE_DIR OST_LIBRARY_BASE)
 
-
-
-if (OST_FOUND)
-    mark_as_advanced(OST_INCLUDE_DIR)
-    mark_as_advanced(OST_LIBRARY_BASE)
-    mark_as_advanced(OST_LIBRARY_INDI)
-    SET(OST_LIBRARIES OST_LIBRARY_INDI OST_LIBRARY_BASE )
-else (OST_FOUND)
-    message(FATAL_ERROR "OST is not found. Please install it first.")
-endif(OST_FOUND)
+IF(OST_FOUND)
+  SET(OST_LIBRARIES ${OST_LIBRARY_BASE})
+ELSE(OST_FOUND)
+  MESSAGE(FATAL_ERROR "OST is not found. Please install it first.")	
+ENDIF(OST_FOUND)
